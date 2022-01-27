53307b6e0e66d6bebc52bb2cf5cc6532e064e4b2
Enakonda <97044520+TuttuRavindra@users.noreply.github.com>
2022-01-27 23:45:36 +0530

    added spring project

---
+++ Test-spring-ptoject/src/main/java/com/test/spring/project/Testspringptoject/TestSpringPtojectApplication.java
@@ -0,0 +1,13 @@
+package com.test.spring.project.Testspringptoject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringPtojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringPtojectApplication.class, args);
+	}
+
+}
---
+++ Test-spring-ptoject/src/test/java/com/test/spring/project/Testspringptoject/TestSpringPtojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.spring.project.Testspringptoject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringPtojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
